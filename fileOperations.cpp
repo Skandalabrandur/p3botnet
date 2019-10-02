9d07ec4076d09e9dc3d33cb5055ecc94651566e6
skandalabrandur <steinerinn@gmail.com>
2019-10-02 16:50:26 +0000

    Working example of a logger in fileOperations.cpp

---
+++ fileOperations.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+using namespace std;
+
+// Reference: http://www.cplusplus.com/doc/tutorial/files/
+// Reference: https://en.cppreference.com/w/cpp/chrono
+
+void writeToLog(std::string text) {
+    //oh boy
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+    ofstream logfile;
+    logfile.open ("log.txt", ios::app);
+    logfile << text << std::endl;
+    logfile << "Timestamp: " << std::ctime(&now) << std::endl;
+    logfile.close();
+}
+
+int main () {
+    writeToLog("This was written, when?");
+    return 0;
+}
