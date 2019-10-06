cc487fd7640c493481f3a565db622bad01570337
skandalabrandur <steinerinn@gmail.com>
2019-10-06 12:20:20 +0000

    Created dataobject structs: s_message and s_server

---
+++ dataObjects.h
@@ -0,0 +1,15 @@
+#ifndef DATA_OBJ_H
+#define DATA_OBJ_H
+#include <string.h>
+
+struct s_message {
+    std::string msg;        // The message contents
+    std::string receiver;   // GROUP_ID, who shall receive?
+}
+
+struct s_server {
+    int fd;
+    std::string group_id;
+}
+
+#endif
